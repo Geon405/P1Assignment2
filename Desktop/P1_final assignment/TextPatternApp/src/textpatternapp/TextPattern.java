ec2eeddee01697de630fb9f35624add1255dc7e8
Geon405 <kg000405@gmail.com>
2020-05-10 16:26:02 -0400

    added a second overloaded constructor

--- Desktop/P1_final assignment/TextPatternApp/src/textpatternapp/TextPattern.java
+++ Desktop/P1_final assignment/TextPatternApp/src/textpatternapp/TextPattern.java
@@ -10,12 +10,19 @@ public class TextPattern {
     private String text;
     private int Pattern;
     
-    public TextPattern()
+    public TextPattern() // the default constructor
     {
         this.text = "AbrAcadAbrA";
     }
     
-    public TextPattern (String text) {
+    public TextPattern (String text) // an overloaded constructor
+    {
         this.text = text;
     }
+    
+    public TextPattern (String text, int pattern) // an overloaded constructor
+    {        
+        this.text = "AbrAcadAbrA";
+        this.text = text;
+    }  
 }
